00e1ce9c3bac5310fc45196abe346091cf50a3a2
EEthanZZ <zycc2727@gmail.com>
2023-09-14 04:43:27 -0400

    subprocess handling

--- mac_change.py
+++ mac_change.py
@@ -5,6 +5,13 @@ new_mac = input("New MAC add> ")
 
 print(f"[+] Changing MAC for {interface} to {new_mac}")
 
+
+"""
 subprocess.call(f"ifconfig {interface} down", shell=True)
 subprocess.call(f"ifconfig {interface} hw ether {new_mac} ", shell=True)
-subprocess.call(f"ifconfig {interface} up {new_mac}", shell=True)
+subprocess.call(f"ifconfig {interface} up", shell=True)
+"""
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
