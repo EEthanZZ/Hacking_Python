d2dde4585b8e49bcaada6992401cd9484650c1f4
EEthanZZ <zycc2727@gmail.com>
2023-09-16 00:46:50 -0400

    set destination MAC to broadcast MAC

--- network_scanner.py
+++ network_scanner.py
@@ -5,7 +5,10 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst = ip)
-    print(arp_request.summary())
-
+    # print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # print(broadcast.summary())
+    arp_broadcast = broadcast/arp_request
+    arp_broadcast.show()
 
 scan("192.168.58.2/24")
