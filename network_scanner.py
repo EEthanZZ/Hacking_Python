3e9a3e2c020c22a277a070197c8de62d53873a92
EEthanZZ <zycc2727@gmail.com>
2023-09-16 01:28:22 -0400

    sned&recieve packets by srp function

--- network_scanner.py
+++ network_scanner.py
@@ -9,6 +9,9 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     # print(broadcast.summary())
     arp_broadcast = broadcast/arp_request
-    arp_broadcast.show()
+    # arp_broadcast.show()
+    (ans,unans) = scapy.srp(arp_broadcast, timeout=1)
+    print(ans.summary())
+
 
 scan("192.168.58.2/24")
