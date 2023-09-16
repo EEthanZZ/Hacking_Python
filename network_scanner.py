a5db68ebbeafd24fe9ab6d77e37101bef5b25a89
EEthanZZ <zycc2727@gmail.com>
2023-09-16 03:02:10 -0400

    parse the response by  dictionary

--- network_scanner.py
+++ network_scanner.py
@@ -11,16 +11,21 @@ def scan(ip):
     arp_broadcast = broadcast/arp_request
     # arp_broadcast.show()
     ans_list = scapy.srp(arp_broadcast, timeout=1, verbose=False)[0]
-    print("IP\t\t\tMAC\n---------------------------------------------")
-    for i in ans_list:
-        print(f"{i[1].hwsrc}\t{i[1].psrc}")
-    print()
-    print()
+    # for i in ans_list:
+    #    print(f"{i[1].hwsrc}\t{i[1].psrc}")
     client_list = []
     for i in ans_list:
-        client_dict = {f"ip: {i[1].psrc}, mac: {i[1].hwsrc}"}
+        client_dict = {"ip": i[1].psrc, "mac": i[1].hwsrc}
         client_list.append(client_dict)
-    print(client_list)
+    return client_list
 
 
-scan("192.168.58.2/24")
+def print_result(result_list2):
+    print("IP\t\t\tMAC\n---------------------------------------------")
+    for i in result_list2:
+        print(f"{i['ip']}\t\t{i['mac']}")
+
+
+result_list = scan("192.168.58.2/24")
+#print(result_list)
+print_result(result_list)
