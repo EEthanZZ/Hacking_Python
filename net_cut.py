2ac0e768af12ed1740ececf8250e4a505391c298
EEthanZZ <zycc2727@gmail.com>
2023-09-19 03:01:47 -0400

    send the DNS require

--- net_cut.py
+++ net_cut.py
@@ -1,12 +1,18 @@
 import netfilterqueue as net
+import scapy.all as scapy
 
 
 def process_packet(packet):
-    print(packet)
+    scapy_packets = scapy.IP(packet.get_payload())
+    if scapy_packets.haslayer(scapy.DNSRR):
+        print(scapy_packets.show())
+    # packet.accept()
     """
     packet.accept()  # the packet will be sent to the target
     packet.drop()  #drop the packet at here
     """
+    # iptables -I FORWARD -j NFQUEUE --queue-num 0
+
 
 queue = net.NetfilterQueue()
 queue.bind(0, process_packet)
