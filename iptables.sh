9b3aead435a80dbdc7d17a543a53665c7d5b5568
EEthanZZ <zycc2727@gmail.com>
2023-09-22 02:30:54 -0400

    bug fix

--- iptables.sh
+++ iptables.sh
@@ -1,5 +1,4 @@
-!#/bin/bash
 sudo iptables --flush
-sudo iptables -I OUTPUT -j NFQUEUE --queue-num 0
 sudo iptables -I INPUT -j NFQUEUE --queue-num 0
-sudo iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 100000
+sudo iptables -I OUTPUT -j NFQUEUE --queue-num 0
+
